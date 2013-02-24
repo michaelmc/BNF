5693f1ef2424663ec18fe70205c74a1093ad7d96
Michael McLaughlin <michaelmc@gmail.com>
2013-02-24 09:30:46 -0500

    Created ProgramGenerator

---
+++ src/bnf/ProgramGenerator.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package bnf;
+
+import java.util.List;
+
+/**
+ * @author mvm
+ *
+ */
+public class ProgramGenerator {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    public static List<String> expand(String term) {
+        return null;
+    }
+    
+    public static void printList(List<String> list) {
+        
+    }
+
+}
